a2c7c4772909d238402fa7b75549c114e07f3530
Alfio Rizzo <alfio.rizzo@esss.se>
2021-02-02 16:11:24 +0100

    update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+
+setup(name="pvValidatorUtils",
+      packages=find_packages(),
+      package_data={'':["*.so"]},
+      version='1.0.0',
+      description='pvValidator Utils Python Wrapper',
+      author='Alfio Rizzo',
+      author_email='alfio.rizzo@ess.eu',
+      license='GPL',
+      platforms = ['Linux','WSL'],
+      scripts=['bin/pvValidator.py']
+)
+
---
+++ src/epicsUtils.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright information and license terms for this software can be
+ * found in the file LICENSE that is included with the distribution
+ */
+
+#include <stdio.h>
+
+#include <iostream>
+#include <map>
+#include <iterator>
+#include <vector>
+#include <string>
+#include <istream>
+#include <fstream>
+#include <sstream>
+
+
+#include <epicsStdlib.h>
+#include <epicsGetopt.h>
+#include <pv/logger.h>
+
+#include <epicsExit.h>
+#include <osiSock.h>
+
+#include <pv/byteBuffer.h>
+#include <pv/serializeHelper.h>
+#include <pv/pvaConstants.h>
+#include <pv/inetAddressUtil.h>
+#include <pv/configuration.h>
+#include <pv/remote.h>
+#include <pv/rpcClient.h>
+#include <pva/client.h>
+
+
+
+using namespace std;
+
+using namespace epics::pvData;
+using namespace epics::pvAccess;
+
+
+
+
+struct ServerEntry {
+    string guid;
+    string protocol;
+    vector<osiSockAddr> addresses;
+    int8 version;
+};
+
+
+
+
+class epicsUtils {
+    public:
+        epicsUtils(string);
+        epicsUtils(bool);
+        epicsUtils();
+        ~epicsUtils();
+        vector<string> pvstringlist;
+        string HasAlias(string);
+        string serverlist;
+        string getServerList();
+        string getVersion;
+        string getAddress;  
+    private:
+#if defined(_WIN32) && !defined(_MINGW)
+        FILE *popen(const char*, const char *);
+        int pclose(FILE *);
+#endif
+        static const char lookup[16];
+        string toHex(int8* , size_t );
+        std::size_t readSize(ByteBuffer* );
+        string deserializeString(ByteBuffer* );
+        typedef map<string, ServerEntry> ServerMap;
+        ServerMap serverMap;
+        bool processSearchResponse(osiSockAddr const & , ByteBuffer & );
+        bool discoverServers(double );
+        bool byGUIDSearch;
+        double timeOut ;
+        
+              
+
+        
+
+};
+
+
+
